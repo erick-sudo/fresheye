ba234d33cc73fc45959945e868b0887f5747da18
Erick Ochieng <erickochieng766@gmail.com>
2023-02-05 08:07:13 +0300

    Rectifying position

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -44,37 +44,39 @@ function Payment({setPaymentStatusHelper}) {
     }
 
     return (
-        <div className="payment-page">
-            {loading ? <Loading /> : null}
-            <div className="payment">
-            <div className="poster">
-                <img src={poster} alt="POSTER"/>
-                <div className="event-calendar">
-                    <span>4TH</span>
-                    <span>Sat</span>
+        <div className="center-page">
+            <div className="payment-page">
+                {loading ? <Loading /> : null}
+                <div className="payment">
+                    <div className="poster">
+                        <img src={poster} alt="POSTER"/>
+                        <div className="event-calendar">
+                            <span>4TH</span>
+                            <span>Sat</span>
+                        </div>
+                    </div>
+                    <div className="payment-form">
+                        <h1>Buy Ticket</h1>
+                        <form onSubmit={handleSubmit}>
+                            <div className="paybill-number">
+                                <div className="mpesa-logo">
+                                    <img src={mpesalogo} alt="Mpesa" />
+                                </div>
+                                <h1>522522</h1>
+                            </div>
+                            <div className="phone-number">
+                                <span className="country-code">+254</span>
+                                <input name="phonenumber" value={phone} type="number" onChange={(e) => {
+                                    if(e.target.value.length <= 9) {
+                                        setNumber((e.target.value))
+                                    }
+                                }} placeholder="796584498" />
+                            </div>
+                            <input type="submit" value="Buy Ticket" />
+                        </form>
+                    </div>
                 </div>
             </div>
-            <div className="payment-form">
-                <h1>Buy Ticket</h1>
-                <form onSubmit={handleSubmit}>
-                    <div className="paybill-number">
-                        <div className="mpesa-logo">
-                            <img src={mpesalogo} alt="Mpesa" />
-                        </div>
-                        <h1>522522</h1>
-                    </div>
-                    <div className="phone-number">
-                        <span className="country-code">+254</span>
-                        <input name="phonenumber" value={phone} type="number" onChange={(e) => {
-                            if(e.target.value.length <= 9) {
-                                setNumber((e.target.value))
-                            }
-                        }} placeholder="796584498" />
-                    </div>
-                    <input type="submit" value="Buy Ticket" />
-                </form>
-            </div>
-        </div>
         </div>
     )
 }
